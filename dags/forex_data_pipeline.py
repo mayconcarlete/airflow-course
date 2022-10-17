45e24e19ac7fd443b0f295e8d591b594ea50c440
Maycon Carlete <maycon.carlete@gmail.com>
2022-10-17 20:06:00 -0300

    feat: forex dag

--- dags/forex_data_pipeline.py
+++ dags/forex_data_pipeline.py
@@ -1,6 +1,20 @@
 from airflow import DAG
+from datetime import datetime, timedelta
+
+default_args = {
+  "owner": "airflow",
+  "email_on_failure": False,
+  "email_on_retry": False,
+  "email": "maycon.carlete@gmail.com",
+  "retries": 1,
+  "retry_delay": timedelta(minutes=5),
+}
 
 with DAG(
-
+  dag_id="forex_data_pipeline",
+  start_date=datetime(2021, 1, 1),
+  schedule_interval="@daily",
+  default_args=default_args,
+  catchup=False
 ) as dag:
   pass
