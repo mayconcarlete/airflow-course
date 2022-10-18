c98708f762457009973f89c0ec07a078bab7d0a2
Maycon Carlete <maycon.carlete@gmail.com>
2022-10-18 09:31:40 -0300

    feat: add http sensor

--- dags/forex_data_pipeline.py
+++ dags/forex_data_pipeline.py
@@ -22,5 +22,8 @@ with DAG(
   is_forex_rates_available = HttpSensor(
     task_id="is_forex_rates_available",
     http_conn_id="forex_api",
-
+    endpoint="marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b",
+    response_check=lambda response: "rates" in response.text,
+    poke_interval=5,
+    timeout=20
   )
